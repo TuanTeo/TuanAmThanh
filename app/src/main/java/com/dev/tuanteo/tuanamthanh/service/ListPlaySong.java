978f03d6d45f08fa092700167fbe6181484769e2
Dell <feckerbg@gmail.com>
2022-01-04 22:11:54 +0700

    Thêm lớp chứa danh sách bài hát muốn phát

---
+++ app/src/main/java/com/dev/tuanteo/tuanamthanh/service/ListPlaySong.java
@@ -0,0 +1,31 @@
+package com.dev.tuanteo.tuanamthanh.service;
+
+import com.dev.tuanteo.tuanamthanh.object.Song;
+
+import java.util.ArrayList;
+
+/**
+ * Lớp chứa danh sách bài hát muốn phát
+ */
+public class ListPlaySong {
+
+    private ArrayList<Song> mPlayList;
+
+    private static ListPlaySong sInstance;
+
+    public static ListPlaySong getInstance() {
+        if (sInstance != null) {
+            return sInstance;
+        } else {
+            return new ListPlaySong();
+        }
+    }
+
+    public ArrayList<Song> getPlayList() {
+        return mPlayList;
+    }
+
+    public void setPlayList(ArrayList<Song> playList) {
+        this.mPlayList = playList;
+    }
+}
